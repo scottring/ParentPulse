3463f47ff1ea70459d881c469201c48a2efb2265
Scott Kaufman <scottring@hotmail.com>
2026-05-11 09:25:26 -0400

    feat(routes): redirect /growth and /kid to new paths

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,25 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/growth",
+        destination: "/experiments",
+        permanent: true,
+      },
+      {
+        source: "/growth/:itemId",
+        destination: "/experiments/:itemId",
+        permanent: true,
+      },
+      {
+        source: "/kid/:personId",
+        destination: "/check-in/:personId",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
